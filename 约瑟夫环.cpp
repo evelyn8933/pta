0292a3f0e6b2998f487877f4d1c586e937e3c411
evelyn8933 <2605038355@qq.com>
2026-09-19 22:32:49 +0800

    数组版本

--- 约瑟夫环.cpp
+++ 约瑟夫环.cpp
@@ -1,45 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
-struct node
-{ int data;
- struct node *next;
-};
+int main(void)
+{
+    int N, p;
+    scanf("%d%d", &N, &p);
+    int arr[3001];
+    int i;
+    for(i=1; i<=N; i++)
+        arr[i] = i;
 
-int main()
-{ int n,m;
-scanf("%d%d",&n,&m);
-struct node *head=NULL,*p1,*p2=NULL;
-for(int i=1;i<=n;i++)
-{
-	p1=(struct node *)malloc(sizeof(struct node));
-	p1->data=i;
-	if(head==NULL) head=p1;
-	else p2->next=p1;
-	p2=p1;
+    int remain = N;   // 当前还剩多少人
+    int pos = 0;      // 当前位置
+    int first = 1;
+
+    while(remain > 0)
+    {
+        // 数p个活着的人，找到要删除的位置
+        int step;
+        for(step = 0; step < p; )
+        {
+            pos = pos + 1;
+            if(pos > N) pos = 1;
+            if(arr[pos] != 0) // 这个人还活着
+                step++;
+        }
+        // pos就是本次出圈的人
+        if(!first) printf(" ");
+        printf("%d", arr[pos]);
+        first = 0;
+        arr[pos] = 0; //标记已出局
+        remain--;
+    }
+    return 0;
 }
-if(p2!=NULL) p2->next=head;
-struct node *pre=p2,*cur=head;
-int cnt=0;
-while(pre!=cur)
-{
-	cnt++;
-	if(cnt==m) 
-	{
-		printf("%d ",cur->data);
-		struct node *p=cur;
-		pre->next=cur->next;
-		cur=cur->next;
-		free(p);
-		cnt=0;
-	}
-	else
-	{pre=cur;
-	cur=cur->next; 
-	}
-}
-printf("%d",pre->data);
-pre=cur=NULL;
-free(pre);
-free(cur);
-return 0;	
-} 
+
